80c6f67c4ec5d42be089104d7bec55fbb2d89602
ramon gonzalez <ramongonzalez@ramons-MacBook-Pro.local>
2024-01-25 21:16:34 -0500

    ultima actualizacion

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,24 +16,25 @@ exports.register = (req, res) => {
 
     const {name, email, password, passwordConfirm} = req.body;
 
-db.query("SELECT email FROM  proyecto1.usuario WHERE email = ?", [email], async(error,results) => {
+db.query("SELECT email FROM Proyecto1.usuario WHERE email = ?", [email], async( error, results) => {
 if (error){
     console.log(error)
 }
 
-if(results.length > 0 ){
+if(results?.length > 0 ) {
 return res.render('register', {
     massage: "that email is alread exist!"
 })
 
 } else if (password != passwordConfirm){
     return res.render('register', {
-        massage: "that email is already in use "
+        massage: "Passwords do not match"
     })
 }
 
 let hashePassword = await bcrypt.hash(password, 8);
 console.log(hashePassword);
+res.send('testing');
 
 
 
