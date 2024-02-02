11bbcddc6a0401c4bcb0f98d120c299cb08af2a7
ramon gonzalez <ramongonzalez@ramons-MacBook-Pro.local>
2024-02-01 22:27:48 -0500

    sigue intento a coneccion de mysql

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const dotenv = require("dotenv");
 const path = require("path");
+const { Console } = require("console");
 // const router = express.Router();
 dotenv.config({ path: './env'})
 const app = express();
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
 
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE
+// });
+
+const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
-});
+}).promise();
 
+
+// Declaración de una función asíncrona
+// async function miFuncion() {
+//     try {
+//       // Código con await
+//       const [rows] = await pool.query('SELECT * FROM USERS');
+//       console.log(rows);
+//     } catch (error) {
+//       console.error('Error:', error);
+//     }
+//   }
+  
+//   // Llamada a la función asíncrona
+//   miFuncion();
+/////////////////////  
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
@@ -28,19 +51,20 @@ app.use(express.json());
 //onsole.log(__dirname);
 app.set('view engine', 'hbs');
 
-db.connect((err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log("connted with mysql");
-    }
-});
+ pool.getConnection((err)=>{
+     if(err){
+         console.log(err)
+     }else{
+                 console.log("connted with mysql");
+     }
+ });
 
 
 //define routes
 
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
+
 
 app.listen(5001, () => {
 console.log("Server listening on port 5001");
